cab79f378c998d95c7fe8e4d4d696a08e25d375f
masraga <github.ragamulia@gmail.com>
2025-11-13 19:44:37 +0700

    chore: add db

---
+++ database.sql
@@ -0,0 +1,21 @@
+-- Adminer 5.4.1 PostgreSQL 16.10 dump
+
+DROP DATABASE IF EXISTS "iykra-test";
+CREATE DATABASE "iykra-test";
+\connect "iykra-test";
+
+DROP TABLE IF EXISTS "employees";
+DROP SEQUENCE IF EXISTS employees_id_seq;
+CREATE SEQUENCE employees_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."employees" (
+    "id" integer DEFAULT nextval('employees_id_seq') NOT NULL,
+    "name" character varying(100) NOT NULL,
+    "position" character varying(70) NOT NULL,
+    "salary" integer NOT NULL,
+    CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
+)
+WITH (oids = false);
+
+
+-- 2025-11-13 12:42:43 UTC
